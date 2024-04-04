71bfbe3871a725135ce32c060b75716f2c26cb49
MGP <z1186786523@qq.com>
2024-04-04 16:45:09 +0800

    Cinemachine 玩家控制 联机功能测试

---
+++ Assets/Scripts/Controller/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Cinemachine;
+using Mirror;
+using UnityEngine;
+
+namespace Controller
+{
+    public class PlayerController : NetworkBehaviour
+    {
+        public float moveSpeed = 5f;
+        
+        private Rigidbody _rb;
+        private CinemachineVirtualCamera _virtualCamera;
+        
+        public override void OnStartLocalPlayer()
+        {
+            _virtualCamera = GameObject.FindObjectOfType<CinemachineVirtualCamera>();
+            _virtualCamera.Follow = transform;
+            _virtualCamera.LookAt = transform;
+        }
+
+        private void Start()
+        {
+            _rb = GetComponent<Rigidbody>();
+        }
+
+        private void Update()
+        {
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
+
+            Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+            _rb.velocity = movement * moveSpeed;
+        }
+        
+    }
+}
